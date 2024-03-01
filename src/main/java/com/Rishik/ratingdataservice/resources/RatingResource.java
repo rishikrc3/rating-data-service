3b5552f215c44fde80b678dd3b331f873499e42f
Rishik <rishikrc3@gmail.com>
2024-03-01 14:20:22 +0530

    setting_up_final_microservice

---
+++ src/main/java/com/Rishik/ratingdataservice/resources/RatingResource.java
@@ -0,0 +1,16 @@
+package com.Rishik.ratingdataservice.resources;
+
+import com.Rishik.ratingdataservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingResource {
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId)
+    {
+        return new Rating(movieId, 4);
+    }
+}
